7d35016cff9d82262eb107cb6db4abff414a50bd
buvanesh <buvanesh1902@gmailcom>
2024-04-18 09:24:53 +0530

    Porfolio Created Successfully

---
+++ main.js
@@ -0,0 +1,7 @@
+let menuIcon=document.querySelector('#menu-icon');
+let navvar=document.querySelector('.navbar');
+
+menuIcon.onclick=()=>{
+     menuIcon.classList.toggle('fa-x')
+     menuIcon.classList.toggle('active')
+}
